ebfca52a2df01f37f3c80df305aee12bf9385f9f
kisung <ppter0311@naver.com>
2022-12-04 19:03:04 +0900

    실습 1. 인사말과 사용자에게 입력 받은 정보 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
     val name = readLine()
     println("""
         감사합니다. 반값습니다. $name 님
-        원하시는 카테고리를 입력해주세요.
+        원하시는 카테고리를 입력해주세요...
         ***===========================***
     """.trimIndent())
 }
